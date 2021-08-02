556a39232ce7b2aa7500c09437976db5db2cc1e8
Connor Mackay <connor.mackay@gmail.com>
2021-08-02 15:44:56 -0400

    implement score keeping

--- views/helpers.js
+++ views/helpers.js
@@ -1,33 +1,46 @@
-window.onload = (event) => {
+
 
 let game_board = [null, null, null, null, null, null, null, null, null];
-
-const game_container = document.getElementById("play_area");
-
-const make_board = () => {
-  if (game_container.innerHTML) {
-    game_container.innerHTML = "";
-  }
-  game_board.forEach((element, index) => {
-    game_container.innerHTML += `<div id="square_${index}" class="game_square">${game_board[index]}</div>`;
-    if (element == playerOne || element == playerTwo) {
-      document.getElementById(`square_${index}`).classList.add("selected")
-    }
-  });
-};
-
+let numTurnsLeft = 9;
+let bluesTurn = true;
 const playerOne = "BLUE";
 const playerTwo = "YELLOW";
 
-const addPlayerMove = (selected, player) => {
+const addPlayerMove = (selected) => {
   if (!game_board[selected]) {
-    game_board[selected] = player;
-    make_board();
+    game_board[selected] = bluesTurn ? playerOne : playerTwo;
+    console.log(game_board);
+    if (bluesTurn) {
+      bluesTurn = false;
+    } else {
+      bluesTurn = true;
+    }
+
   }
 };
 
 
-  console.log('page is fully loaded');
+window.onload = (event) => {
+
+  const game_container = document.getElementById("play_area");
+
+
+  const make_board = () => {
+    if (game_container.innerHTML) {
+      game_container.innerHTML = "";
+    }
+    game_board.forEach((element, index) => {
+      game_container.innerHTML += 
+      `<div id="square_${index}" class="game_square" onClick="addPlayerMove(${index})">
+        ${game_board[index]}
+      </div>`;
+      if (element == playerOne || element == playerTwo) {
+        document.getElementById(`square_${index}`).classList.add("selected")
+      }
+    });
+  };
+
+
   make_board();
 };
 
