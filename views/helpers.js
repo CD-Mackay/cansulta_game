6b27b4e63a3609372ffef0b90086b1381b456a74
Connor Mackay <connor.mackay@gmail.com>
2021-08-02 15:26:50 -0400

    build player turn function

--- views/helpers.js
+++ views/helpers.js
@@ -1,21 +1,33 @@
 window.onload = (event) => {
-  
-let game_board = ["", "", "", "", "", "", "", "", ""];
+
+let game_board = [null, null, null, null, null, null, null, null, null];
 
 const game_container = document.getElementById("play_area");
 
-const reset_board = () => {
-  console.log('resettin the board!')
-  if (game_container.innerHTML !== null) {
+const make_board = () => {
+  if (game_container.innerHTML) {
     game_container.innerHTML = "";
   }
   game_board.forEach((element, index) => {
     game_container.innerHTML += `<div id="square_${index}" class="game_square">${game_board[index]}</div>`;
+    if (element == playerOne || element == playerTwo) {
+      document.getElementById(`square_${index}`).classList.add("selected")
+    }
   });
+};
+
+const playerOne = "BLUE";
+const playerTwo = "YELLOW";
+
+const addPlayerMove = (selected, player) => {
+  if (!game_board[selected]) {
+    game_board[selected] = player;
+    make_board();
+  }
 };
 
 
   console.log('page is fully loaded');
-  reset_board();
+  make_board();
 };
 
