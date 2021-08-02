bd3ae6740d7b4caa0fec0e39918f761bf704539a
Connor Mackay <connor.mackay@gmail.com>
2021-08-02 14:34:13 -0400

    implement reset board script

--- views/helpers.js
+++ views/helpers.js
@@ -3,10 +3,20 @@ let game_board = [null, null, null, null, null, null, null, null, null];
 const game_container = document.getElementsByClassName('play_area');
 
 const reset_board = () => {
+  console.log('resettin the board!')
   game_container.innerHTML = "";
-  game_board.map((element, index) => {
-    game_container.innerHTML += `<div id="square_${index}" class="game_square">${game_board[index]}</div>`
+  game_board.forEach((element, index) => {
+    console.log(game_container.innerHTML);
+    game_container.innerHTML += `<div id="square_${index}" class="game_square">${game_board[index]}</div>`;
   });
 };
+console.log(game_container);
 
-reset_board();
+window.onload = (event) => {
+  console.log('page is fully loaded');
+
+  
+  reset_board();
+  console.log(game_container.innerHTML);
+};
+
